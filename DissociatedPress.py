c85510bf5f42dfac03f3bf06630dfd6f14cf18f7
Chris Hodapp <Hodapp87@gmail.com>
2014-03-11 19:51:32 -0400

    Made Python 2.7/3.x compatible.

--- DissociatedPress.py
+++ DissociatedPress.py
@@ -15,12 +15,14 @@ import random
 # when your text really can't all be fit in memory at once
 # (6) Letter-based version rather than word-based
 
-def dissociated_press_from_file(fname, chunk_size, chunks):
-    """Run the Dissociated Press algorithm on the text in the given file.
+def dissociated_press_from_file(fnames, chunk_size, chunks):
+    """Run the Dissociated Press algorithm on the text in the given files.
     All other behavior is identical to the dissociated_press() function."""
-    f = open(fname)
-    text = f.read()
-    f.close()
+    text = ""
+    for fname in fnames:
+        f = open(fname)
+        text += f.read()
+        f.close()
     return dissociated_press(text, chunk_size, chunks)
 
 def dissociated_press(text, chunk_size, chunks):
@@ -32,49 +34,46 @@ def dissociated_press(text, chunk_size, chunks):
 
     Thus, the result will have around (chunk_size * chunks) words in it.
     Newlines will be removed.
-
     """
     text_clean = text.replace("\n", " ").replace("\r", "")
     # Separate into a list of words, and remove empty spaces.
-    words = filter(None, text_clean.split(" "))
+    words = list(filter(None, text_clean.split(" ")))
     # Pick 'chunk_size' consecutive random words.
     start_index = random.randrange(0, len(words) - chunk_size + 1)
     end_index = start_index + chunk_size
     seed = words[start_index:end_index]
-    str = " ".join(seed)
+    strs = seed
     total_choices = 1
     for i in range(chunks):
         seed, match_count = dissociated_press_iterate(words, seed[-chunk_size:])
         total_choices *= match_count
-        str += " " + " ".join(seed)
+        strs += seed
     # This can provide some insight into how "random" the generated text was:
-    #print total_choices
-    return str
+    #print(total_choices)
+    return " ".join(strs)
 
 def dissociated_press_iterate(words, last_chunk, chunk_size = -1):
     # Assume last_chunk is the chunk size
     size = len(last_chunk)
-    matches = find_in_list_consecutive(words, last_chunk)
+    matches = list(find_in_list_consecutive(words, last_chunk))
     # Pick a random match.
     start = random.choice(matches)
     return words[start + size:start + size*2], len(matches)
 
 def find_in_list_consecutive(l, pattern):
-    """Find a pattern inside of a list. Returns a list of indices to 'l' where
+    """Find a pattern inside of a list. Yields indices to 'l' where
     'pattern' is found.
 
     l -- The list (or list-like object) inside of which to search
     pattern -- The list (or list-like object) to find inside of 'l'. It must
     appear in order, consecutively and in entirety.
-
     """
     # (Python probably already has a better, more Pythonic way to do this. I
     # don't know what it is though.)
     # This case is an automatic failure:
     if len(pattern) > len(l):
-        return []
+        return
     # Iterate over matches for the first element.
-    matches = []
     for element in pattern:
         # s = start index. -1 is an initial value.
         s = -1
@@ -92,22 +91,22 @@ def find_in_list_consecutive(l, pattern):
                         break
                     all_matched = all_matched and l[s + i] == pattern[i]
                     i += 1
-                if all_matched: matches.append(s)
+                if all_matched:
+                    yield s
         except ValueError:
-            # Not even the first item has matched, so we're done.
-            return matches
+            pass
 
 def main(argv):
     if (len(argv) < 3):
-        print "Dissociated Press implementation"
-        print '("An algorithm for generating text based on another text.")'
-        print "Usage: %s input_file chunk_size chunks" % argv[0]
-        print "Final text will be about chunk_size * chunks words long."
+        print("Dissociated Press implementation")
+        print('("An algorithm for generating text based on another text.")')
+        print("Usage: %s <chunk_size> <chunks> <input_file> [input_file2...]" % argv[0])
+        print("Final text will be about chunk_size * chunks words long.")
         return -1
-    fname = argv[1]
-    chunk_size = int(argv[2])
-    chunks = int(argv[3])
-    print dissociated_press_from_file(fname, chunk_size, chunks)
+    fnames = argv[3:]
+    chunk_size = int(argv[1])
+    chunks = int(argv[2])
+    print(dissociated_press_from_file(fnames, chunk_size, chunks))
 
 import sys
 main(sys.argv)
